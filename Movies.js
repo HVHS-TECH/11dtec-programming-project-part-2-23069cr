5e831ffbd3b8528355518b2b58b0adc8d5512537
23069cr <23069cr@hvhs.school.nz>
2025-06-12 00:38:19 +0000

    Jun 12, 2025, 12:38 AM

--- Movies.js
+++ Movies.js
@@ -5,14 +5,15 @@ let comedy = ["Ace Ventura: Pet Detective", "The Mask", "The Cable Guy", "Dumb a
 let romcom = ["Pretty Woman", "The Proposal", "Love Actually", "Some Like It Hot", "Just like Heaven", "Crazy , Stupid, Love", "10 Thing I Hate About You"];
 let Horror = ["Child's Play", "IT", "Friday The 13th", "Nightmare On Elm Street", "Saw", "Halloween", "The Texas Chainsaw Massacre"];
 let fanatsy = ["Harry Potter and the Prisoner of Azkaban", "The Lord of the Rings: The Fellowship of the Ring", "Labyrinth", "The Never Ending Story", "Wicked", "The World's End", "How To Train Your Dragon"];
+let familyfriendly = ["Scooby Doo", "", "Transformers", "Die Hard", "Bad Boys", "Commando", "Top Gun"];
   var userMovie = prompt("What movie do you need?");
 
   while (userMovie == null || userMovie == "" || userMovie == " ") {
     userMovie = prompt("Please enter a movie. \n What movie do you need?");
   }
-  if userMovie
+  if userMovie{
   alert("You are now renting " + userMovie + "\n Price: " )
-
+}
   var club = prompt("Would you like a Club Penguin membership card to go along with your purchase?\nYes or no")
 
   while (club == null || club =="" || club == " ") {
