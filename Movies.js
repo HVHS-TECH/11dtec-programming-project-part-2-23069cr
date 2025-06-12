08d840c1a70f4843c35fdf322cf7a667d1d8c5f7
23069cr <23069cr@hvhs.school.nz>
2025-06-12 00:51:05 +0000

    Jun 12, 2025, 12:51 AM

--- Movies.js
+++ Movies.js
@@ -1,12 +1,12 @@
 function start() {
-
+let genre = [" Action", " Comedy", " RomCom", " Horror", " Fantasy", " Family Friendly"]
 let action = ["Terminator", "John Wick", "Transformers", "Die Hard", "Bad Boys", "Commando", "Top Gun"];
 let comedy = ["Ace Ventura: Pet Detective", "The Mask", "The Cable Guy", "Dumb and Dumber", "Lair Lair", "Me, Myself and Irene", "Bruce Almighty"];
-let romcom = ["Pretty Woman", "The Proposal", "Love Actually", "Some Like It Hot", "Just like Heaven", "Crazy , Stupid, Love", "10 Thing I Hate About You"];
+let romcom = ["Pretty Woman", "The Proposal", "Love Actually", "Some Like It Hot", "Just like Heaven", "Crazy, Stupid, Love", "10 Thing I Hate About You"];
 let Horror = ["Child's Play", "IT", "Friday The 13th", "Nightmare On Elm Street", "Saw", "Halloween", "The Texas Chainsaw Massacre"];
 let fanatsy = ["Harry Potter and the Prisoner of Azkaban", "The Lord of the Rings: The Fellowship of the Ring", "Labyrinth", "The Never Ending Story", "Wicked", "The World's End", "How To Train Your Dragon"];
 let familyfriendly = ["Scooby Doo", "Shrek", "The Lion King", "How The Grinch Stole Christmas", "Paddington", "Frozen", "Spider-Man: Into The Spider-Verse"];
-  var userMovie = prompt("What genre do you need?");
+  var userMovie = prompt("What genre do you need?" +genre);
 
   while (userMovie == null || userMovie == "" || userMovie == " ") {
     userMovie = prompt("Please enter a movie. \n What movie do you need?");
