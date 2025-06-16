62e7a37f28aba336173e3321a52d34b39d731d7f
23069cr <23069cr@hvhs.school.nz>
2025-06-16 21:59:14 +0000

    making it more simpler

--- Movies.js
+++ Movies.js
@@ -4,55 +4,39 @@ let action = [" Terminator", " John Wick", " Transformers", " Die Hard", " Bad B
 let comedy = [" Ace Ventura: Pet Detective", " The Mask", " The Cable Guy", " Dumb and Dumber", " Lair Lair", " Me, Myself and Irene", " Bruce Almighty"];
 let romcom = [" Pretty Woman", " The Proposal", " Love Actually", " Some Like It Hot", " Just like Heaven", " Crazy, Stupid, Love", " 10 Thing I Hate About You"];
 let Horror = [" Child's Play", " IT", " Friday The 13th", " Nightmare On Elm Street", " Saw", " Halloween", " The Texas Chainsaw Massacre"];
-let fanatsy = ["Harry Potter and the Prisoner of Azkaban", "The Lord of the Rings: The Fellowship of the Ring", "Labyrinth", "The Never Ending Story", "Wicked", "The World's End", "How To Train Your Dragon"];
-let familyfriendly = ["Scooby Doo", "Shrek", "The Lion King", "How The Grinch Stole Christmas", "Paddington", "Frozen", "Spider-Man: Into The Spider-Verse"];
-  var userGenre = prompt("What genre do you need?" +genre);
-while (userGenre == null || userGenre == "" || userGenre == " ") {
-    userGenre = prompt("Please enter a genre. \n What genre do you need?" +genre);
-  }
+let fanatsy = [" Harry Potter and the Prisoner of Azkaban", "The Lord of the Rings: The Fellowship of the Ring", "Labyrinth", "The Never Ending Story", "Wicked", "The World's End", "How To Train Your Dragon"];
+let familyfriendly = [" Scooby Doo", "Shrek", "The Lion King", "How The Grinch Stole Christmas", "Paddington", "Frozen", "Spider-Man: Into The Spider-Verse"];
+ 
+
+
+
+
 
   /*
   Action starting
   */
-var userMovieAction = ""
-  console.log(userMovieAction)
-  if (userGenre == "Action"){
-    userMovieAction = prompt("Great what action movie would you like? Choose from here" +action)
+var userMovie = prompt("What movie do you need?")
+while (userMovie == null || userMovie == "" || userMovie == " ") {
+    userGenre = prompt("Please enter a movie");
   }
-   console.log("first ask result: " + userMovieAction)
-  while (userMovieAction !== "Terminator" && userMovieAction !== "John Wick" && userMovieAction !== "Transformers" && userMovieAction !== "Die Hard" && userMovieAction !== "Bad Boys" && userMovieAction !== "Commando" && userMovieAction !== "Top Gun" || userMovieAction == null || userMovieAction == "" || userMovieAction == " "){
-    userMovieAction = prompt("Either you typed the name incorrectly or we don't have that movie, please choose one of these here action movies." +action)
-    console.log("while trap: " + userMovieAction)
+   console.log("first ask result: " + userMovie)
+  while (userMovie !== "Terminator" && userMovie !== "John Wick" && userMovie !== "Transformers" && userMovie !== "Die Hard" && userMovie !== "Bad Boys" && userMovie !== "Commando" && userMovie !== "Top Gun" && userMovie !== "Ace Ventura: Pet Detective" && userMovie !== "The Mask" && userMovie !== "The Cable Guy" && userMovie !== "Dumb and Dumber" && userMovie !== "Lair Lair" && userMovie !== "Me, Myself and Irene" && userMovie !== "Bruce Almighty" && userMovie == null || userMovie == "" || userMovie == " "){
+    userMovie = prompt("Either you typed the name incorrectly or we don't have that movie, please choose one of these here action movies." +action)
+    console.log("while trap: " + userMovien)
   }
-  console.log("Checking: " + userMovieAction)
-if (userMovieAction == "Terminator" || userMovieAction == "John Wick" || userMovieAction == "Transformers" || userMovieAction == "Die Hard" || userMovieAction == "Bad Boys" || userMovieAction == "Commando" || userMovieAction == "Top Gun"){
-    userMovieAction = alert("You are now renting" +userMovieAction)
+  console.log("Checking: " + userMovie)
+if (userMovie == "Terminator" || userMovie == "John Wick" || userMovie == "Transformers" || userMovie == "Die Hard" || userMovie == "Bad Boys" || userMovie == "Commando" || userMovie == "Top Gun"){
+    userMovie = alert("You are now renting the action movie" +userMovie)
+  }
+  if (userMovie == "Ace Ventura: Pet Detective" || userMovie == "The Mask" || userMovie == "The Cable Guy" || userMovie == "Dumb and Dumber" || userMovie == "Lair Lair" || userMovie == "Me, Myself and Irene" || userMovie == "Bruce Almighty"){
+    userMovie = alert("You are now renting the comedy movie" +userMovie)
   }
 /*
   action ends
 */
  
 
-  /*
-  Comedy starting
-  */
-var userMovieComedy = ""
-  console.log(userMovieComedy)
-  if (userGenre == "Comedy"){
-    userMovieComedy = prompt("Great what action movie would you like? Choose from here" +comedy)
-  }
-   console.log("first ask result: " + userMovieComedy)
-  while (userMovieComedy !== "Ace Ventura: Pet Detective" && userMovieComedy !== "The Mask" && userMovieComedy !== "The Cable Guy" && userMovieComedy !== "Dumb and Dumber" && userMovieComedy !== "Lair Lair" && userMovieComedy !== "Me, Myself and Irene" && userMovieComedy !== "Bruce Almighty" || userMovieComedy == null || userMovieComedy == "" || userMovieComedy == " "){
-    userMovieAction = prompt("Either you typed the name incorrectly or we don't have that movie, please choose one of these here action movies." +action)
-    console.log("while trap: " + userMovieAction)
-  }
-  console.log("Checking: " + userMovieAction)
-if (userMovieComedy == "Ace Ventura: Pet Detective" || userMovieComedy == "The Mask" || userMovieComedy == "The Cable Guy" || userMovieComedy == "Dumb and Dumber" || userMovieComedy == "Lair Lair" || userMovieComedy == "Me, Myself and Irene" || userMovieComedy == "Bruce Almighty"){
-    userMovieComedy = alert("You are now renting" +userMovieComedy)
-  }
-/*
-  comedy ends
-*/
+
 
   var club = prompt("Would you like a Club Penguin membership card to go along with your purchase?\nYes or no")
 
