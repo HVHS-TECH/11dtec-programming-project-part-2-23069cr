9712c12e5d6cdffcef7341bf5084591a23e1b7f2
23069cr <23069cr@hvhs.school.nz>
2025-06-18 23:39:15 +0000

    Jun 18, 2025, 11:39 PM

--- Movies.js
+++ Movies.js
@@ -30,7 +30,7 @@ var userMovie = prompt("What movie do you need? \n \nAction:" +action+ "\n \nCom
   }
 
   console.log("Checking: " + userMovie)
-if (){
+if (action.includes(userMovie)){
     userMovie = alert("You are now renting the action movie: \n" +userMovie+ "\nPrice:" +actionPrice) 
   }
 
