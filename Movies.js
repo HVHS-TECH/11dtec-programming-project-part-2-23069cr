165af9b20f854d5bb0680218f8172bae43458ed6
23069cr <23069cr@hvhs.school.nz>
2025-06-18 00:08:52 +0000

    Jun 18, 2025, 12:08 AM

--- Movies.js
+++ Movies.js
@@ -4,8 +4,8 @@ let action = [" Terminator", " John Wick", " Transformers", " Die Hard", " Bad B
 let comedy = [" Ace Ventura: Pet Detective", " The Mask", " The Cable Guy", " Dumb and Dumber", " Lair Lair", " Me, Myself and Irene", " Bruce Almighty"];
 let romcom = [" Pretty Woman", " The Proposal", " Love Actually", " Some Like It Hot", " Just like Heaven", " Crazy, Stupid, Love", " 10 Thing I Hate About You"];
 let Horror = [" Child's Play", " IT", " Friday The 13th", " Nightmare On Elm Street", " Saw", " Halloween", " The Texas Chainsaw Massacre"];
-let fantasy = [" Harry Potter and the Prisoner of Azkaban", "The Lord of the Rings: The Fellowship of the Ring", "Labyrinth", "The Never Ending Story", "Wicked", "The World's End", "How To Train Your Dragon"];
-let familyfriendly = [" Scooby Doo", "Shrek", "The Lion King", "How The Grinch Stole Christmas", "Paddington", "Frozen", "Spider-Man: Into The Spider-Verse"];
+let fantasy = [" Harry Potter and the Prisoner of Azkaban", " The Lord of the Rings: The Fellowship of the Ring", " Labyrinth", " The Never Ending Story", " Wicked", " The World's End", " How To Train Your Dragon"];
+let familyfriendly = [" Scooby Doo", " Shrek", " The Lion King", " How The Grinch Stole Christmas", " Paddington", " Frozen", " Spider-Man: Into The Spider-Verse"];
  
 
 
