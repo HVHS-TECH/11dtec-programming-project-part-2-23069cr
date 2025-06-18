b18615014842c15076b9e1b36b83e3c7066b9322
23069cr <23069cr@hvhs.school.nz>
2025-06-18 23:44:42 +0000

    Jun 18, 2025, 11:44 PM

--- Movies.js
+++ Movies.js
@@ -52,18 +52,7 @@ function start() {
 
 
 
-  var club = prompt("Would you like a Club Penguin membership card to go along with your purchase?\nYes or no")
 
-  while (club == null || club == "" || club == " ") {
-    club = prompt("Decide, yes or no");
-  }
-  if (club == "Yes" || club == "yes") {
-    alert("Here you go \n Price: ")
-  }
-  if (club == "No" || club == "no") {
-    alert("I won't be able to feed my kids tonight thanks to you.")
-  }
-}
 
 /*
 | ||
