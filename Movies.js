4f38625ec39d8ba9846dff8a8aad51ebb59ab94f
23069cr <23069cr@hvhs.school.nz>
2025-06-18 00:07:49 +0000

    Jun 18, 2025, 12:07 AM

--- Movies.js
+++ Movies.js
@@ -4,7 +4,7 @@ let action = [" Terminator", " John Wick", " Transformers", " Die Hard", " Bad B
 let comedy = [" Ace Ventura: Pet Detective", " The Mask", " The Cable Guy", " Dumb and Dumber", " Lair Lair", " Me, Myself and Irene", " Bruce Almighty"];
 let romcom = [" Pretty Woman", " The Proposal", " Love Actually", " Some Like It Hot", " Just like Heaven", " Crazy, Stupid, Love", " 10 Thing I Hate About You"];
 let Horror = [" Child's Play", " IT", " Friday The 13th", " Nightmare On Elm Street", " Saw", " Halloween", " The Texas Chainsaw Massacre"];
-let fanatsy = [" Harry Potter and the Prisoner of Azkaban", "The Lord of the Rings: The Fellowship of the Ring", "Labyrinth", "The Never Ending Story", "Wicked", "The World's End", "How To Train Your Dragon"];
+let fantasy = [" Harry Potter and the Prisoner of Azkaban", "The Lord of the Rings: The Fellowship of the Ring", "Labyrinth", "The Never Ending Story", "Wicked", "The World's End", "How To Train Your Dragon"];
 let familyfriendly = [" Scooby Doo", "Shrek", "The Lion King", "How The Grinch Stole Christmas", "Paddington", "Frozen", "Spider-Man: Into The Spider-Verse"];
  
 
@@ -15,11 +15,11 @@ let familyfriendly = [" Scooby Doo", "Shrek", "The Lion King", "How The Grinch S
   /*
   Action starting
   */
-var userMovie = prompt("What movie do you need? \nAction:" +action+ "\nComedy:" +comedy);
+var userMovie = prompt("What movie do you need? \n \n Action:" +action+ "\n \n Comedy:" +comedy+ "\n \n RomCom:" +romcom+ "\n \n Horror:" +Horror+ "\n \n Fantasy:" +fantasy+ "\n \n Family Friendly" +familyfriendly);
 
    console.log("first ask result: " + userMovie)
   while (userMovie !== "Terminator" && userMovie !== "John Wick" && userMovie !== "Transformers" && userMovie !== "Die Hard" && userMovie !== "Bad Boys" && userMovie !== "Commando" && userMovie !== "Top Gun" && userMovie !== "Ace Ventura: Pet Detective" && userMovie !== "The Mask" && userMovie !== "The Cable Guy" && userMovie !== "Dumb and Dumber" && userMovie !== "Lair Lair" && userMovie !== "Me, Myself and Irene" && userMovie !== "Bruce Almighty" && userMovie == null || userMovie == "" || userMovie == " "){
-    userMovie = prompt("Either you typed the name incorrectly or we don't have that movie, please choose one of these here action movies." +action)
+    userMovie = prompt("Either you typed the name incorrectly or we don't have that movie, please choose one of these here movies.\n Action:" +action+ "\n Comedy:" +comedy)
     console.log("while trap: " + userMovie)
   }
   console.log("Checking: " + userMovie)
