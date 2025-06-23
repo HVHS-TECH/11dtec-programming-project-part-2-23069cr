1ab75989d15cdb527368e06d49063d37a4f87533
23069cr <23069cr@hvhs.school.nz>
2025-06-23 21:42:28 +0000

    MOVIES

--- Movies.js
+++ Movies.js
@@ -1,12 +1,12 @@
 function start() {
   let genre = [" Action", " Comedy", " RomCom", " Horror", " Fantasy", " Family Friendly"]
   let action = ["Terminator", "John Wick", "Transformers", "Die Hard", "Bad Boys", "Commando", "Top Gun"];
-  let comedy = [" Ace Ventura: Pet Detective", " The Mask", " The Cable Guy", " Dumb and Dumber", " Lair Lair", " Me, Myself and Irene", " Bruce Almighty"];
-  let romcom = [" Pretty Woman", " The Proposal", " Love Actually", " Some Like It Hot", " Just like Heaven", " Crazy, Stupid, Love", " 10 Thing I Hate About You"];
-  let horror = [" Child's Play", " IT", " Friday The 13th", " Nightmare On Elm Street", " Saw", " Halloween", " The Texas Chainsaw Massacre"];
-  let fantasy = [" Harry Potter and the Prisoner of Azkaban", " The Lord of the Rings: The Fellowship of the Ring", " Labyrinth", " The Never Ending Story", " Wicked", " The World's End", " How To Train Your Dragon"];
-  let familyfriendly = [" Scooby Doo", " Shrek", " The Lion King", " How The Grinch Stole Christmas", " Paddington", " Frozen", " Spider-Man: Into The Spider-Verse"];
-
+  let comedy = ["Ace Ventura: Pet Detective", "The Mask", "The Cable Guy", "Dumb and Dumber", "Lair Lair", "Me, Myself and Irene", "Bruce Almighty"];
+  let romcom = ["Pretty Woman", "The Proposal", "Love Actually", "Some Like It Hot", "Just like Heaven", "Crazy, Stupid, Love", "10 Thing I Hate About You"];
+  let horror = ["Child's Play", "IT", "Friday The 13th", "Nightmare On Elm Street", "Saw", "Halloween", "The Texas Chainsaw Massacre"];
+  let fantasy = ["Harry Potter and the Prisoner of Azkaban", "The Lord of the Rings: The Fellowship of the Ring", " Labyrinth", "The Never Ending Story", "Wicked", "The World's End", "How To Train Your Dragon"];
+  let familyfriendly = ["Scooby Doo", "Shrek", "The Lion King", "How The Grinch Stole Christmas", "Paddington", "Frozen", "Spider-Man: Into The Spider-Verse"];
+userPrice = 0;
   let actionPrice = 15;
   let comedyPrice = 10;
   let romcomPrice = 2;
@@ -31,29 +31,47 @@ function start() {
 
   console.log("Checking: " + userMovie);
   if (action.includes(userMovie)) {
-    alert("You are now renting the action movie: \n" + userMovie + "\nPrice:" + actionPrice);
+    alert("You are now renting the action movie: \n" + userMovie + "\nPrice:" +actionPrice);
   }
 
-  if (userMovie == "Ace Ventura: Pet Detective" || userMovie == "The Mask" || userMovie == "The Cable Guy" || userMovie == "Dumb and Dumber" || userMovie == "Lair Lair" || userMovie == "Me, Myself and Irene" || userMovie == "Bruce Almighty") {
-    userMovie = alert("You are now renting the comedy movie: \n" + userMovie)
+  if (comedy.includes(userMovie)) {
+    userMovie = alert("You are now renting the comedy movie: \n" + userMovie + "\nPrice:" +comedyPrice);
   }
 
-  if (userMovie == "Pretty Woman" || userMovie == "The Proposal" || userMovie == "Love Actually" || userMovie == "Some Like It Hot" || userMovie == "Just like Heaven" || userMovie == "Crazy, Stupid, Love" || userMovie == "10 Thing I Hate About You") {
-    userMovie = alert("You are now renting the RomCom movie: \n" + userMovie)
+  if (romcom.includes(userMovie)) {
+    userMovie = alert("You are now renting the RomCom movie: \n" + userMovie + "\nPrice:" +romcomPrice);
   }
 
-  if (userMovie == "Child's Play" || userMovie == "IT" || userMovie == "Friday The 13th" || userMovie == "Nightmare On Elm Street" || userMovie == "Saw" || userMovie == "Halloween" || userMovie == "The Texas Chainsaw Massacre") {
-    userMovie = alert("You are now renting the Horror movie: \n" + userMovie)
+  if (horror.includes(userMovie)) {
+    userMovie = alert("You are now renting the Horror movie: \n" + userMovie + "\nPrice:" +horrorPrice);
   }
+
+  if (fantasy.includes(userMovie)) {
+    userMovie = alert("You are now renting the Fantasy movie: \n" + userMovie + "\nPrice:" +fantasyPrice);
+  }
+
+  if (familyfriendly.includes(userMovie)) {
+    userMovie = alert("You are now renting the Horror movie: \n" + userMovie + "\nPrice:" +familyFriendlyPrice);
+  }
+
+  var club = prompt("Would you like a Club Penguin membership card to go along with your purchase? It's only an extra $5\nYes or no");
+
+  while (club == null || club =="" || club == " ") {
+    club = prompt("Decide, yes or no");
+  }
+  if (club == "Yes" || club == "yes"){
+    alert("Here you go \nrice:" +);
+  } 
+if (club == "No" || club == "no") {
+  alert("I won't be able to feed my kids tonight thanks to you.");
+}
+}
   /*
     action ends
   */
 
 
 
-
-
-}
 
 /*
 | ||
