5b8433f51493340d7531e7bfba90247e5353f87b
23069cr <23069cr@hvhs.school.nz>
2025-06-29 22:15:49 +0000

    Jun 29, 2025, 10:15 PM

--- Movies.js
+++ Movies.js
@@ -1,5 +1,5 @@
 function start() {
-  let genre = [" Action", " Comedy", " RomCom", " Horror", " Fantasy", " Family Friendly"]
+  let genre = ["Action", "Comedy", "RomCom", "Horror", "Fantasy", "Family Friendly"]
   let action = ["Terminator", "John Wick", "Transformers", "Die Hard", "Bad Boys", "Commando", "Top Gun"];
   let comedy = ["Ace Ventura: Pet Detective", "The Mask", "The Cable Guy", "Dumb and Dumber", "Lair Lair", "Me, Myself and Irene", "Bruce Almighty"];
   let romcom = ["Pretty Woman", "The Proposal", "Love Actually", "Some Like It Hot", "Just like Heaven", "Crazy, Stupid, Love", "10 Thing I Hate About You"];
@@ -13,17 +13,12 @@ function start() {
   let fantasyPrice = 5;
   let familyFriendlyPrice = 8;
 
-alert("you chose" +)
+  let message = "You chose " +genres.value+ " here are the movies:"
+     for (i = 0; i < action.length; i++) {
+    message = message + "\nMovie " + i +" is " +action[i];
+     }
+     alert(message)
 }
-
-  /*
-  Action starting
-  */
-
-
-  /*
-    action ends
-  */
 
 
 
